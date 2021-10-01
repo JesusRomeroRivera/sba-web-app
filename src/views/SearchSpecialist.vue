f9a4fafea8d4588a773dfab1a25c4d3f1d1e4865
CarlosIzarra09 <eduizarra2@hotmail.com>
2021-09-30 21:24:58 -0500

    feat: Add SearchSpecialist Page

---
+++ src/views/SearchSpecialist.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="Search">
+    <px-carrousell id="inicio"></px-carrousell>
+
+    <px-ads></px-ads>
+    <px-specialists-list></px-specialists-list>
+    <px-contact></px-contact>
+  </section>
+</template>
+
+<script>
+import PxCarrousell from "@/components/SearchSpecialist/PxCarrousell.vue";
+//import PxAbout from "@/components/Home/PxAbout.vue";
+import PxAds from "@/components/SearchSpecialist/PxAds.vue";
+//import PxList from "@/components/Home/PxList.vue";
+import PxSpecialistsList from "@/components/SearchSpecialist/PxSpecialistsList";
+import PxContact from "@/components/Home/PxContact.vue";
+
+export default {
+  name: "Home",
+  components: {
+    PxCarrousell,
+    //PxAbout,
+    PxSpecialistsList,
+    PxAds,
+    //PxList,
+    PxContact,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+.Home {
+}
+</style>
